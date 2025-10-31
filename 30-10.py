e0003d4b8b46be73b9b4d37a94c28897d662deeb
imshubham22apr-gif <imshubham.22apr@gmail.com>
2025-10-31 15:31:11 +0530

    new code

--- 30-10.py
+++ 30-10.py
@@ -52,22 +52,85 @@ class BankAccount:
     #         print("withdrawal successful")
             
 
-    def withdraw(self,withdrawled_amount):
+#     def withdraw(self,withdrawled_amount):
 
+#         try:
+#             withdrawled_amount=float(input("enter amount to withdraw:"))
+#             self.balance-=withdrawled_amount
+#             if self.balance<withdrawled_amount:
+#                 raise InsufficientBalance("you dont have sufficient balance")
+#             else:
+#                 print("withdrawal successful")
+#         except InsufficientBalance as e:
+#             print(e)
+            
+            
+# p1=BankAccount(123456789,'aashi',50000)
+# p1.withdraw(10000)
+
+# p2=BankAccount(23904,'rhoti',5000)
+# p2.withdraw(9000)
+
+
+#------------------------------------------
+
+class BankAccount:
+    def __init__(self,acc_no,balance=0):
+        self.balance=balance
+        self.acc_no=acc_no
+
+    def withdraw(self,amount):
+        if amount >  self.balance:
+            raise RuntimeError('insufficient balance')
+        if amount<0:
+            raise RuntimeError('invalid amount')
+        self.balance-=amount
+        print(f'withdrawal successful, remaining balance is{self.balance}')
+        
+p1=BankAccount(500)
+try:
+    try:
         try:
-            withdrawled_amount=float(input("enter amount to withdraw:"))
-            self.balance-=withdrawled_amount
-            if self.balance<withdrawled_amount:
-                raise InsufficientBalance("you dont have sufficient balance")
-            else:
-                print("withdrawal successful")
-        except InsufficientBalance as e:
+            p1.withdraw(501)
+        except RuntimeError as e:
             print(e)
-            
-            
-p1=BankAccount(123456789,'aashi',50000)
-p1.withdraw(10000)
+    except ValueError as e:
+        print(e)
+except Exception as e:
+    print('something went wrong')
 
-p2=BankAccount(23904,'rhoti',5000)
-p2.withdraw(9000)
+
+
+class person:
+    def __init__(self):
+        self.name='amam'
+        self.__pin='2934'
+
+    def print_info(self) :
+        print(self.name)
+    def __display_pin(self):
+        print(self.__pin)
+
+
+p1=person()
+p1.print_info()
+# p1.__display_pin()
+
+
+#---------------------------------
+#getter function
+def get_atmpin(self):
+    print(self.__atmpin)
+    
+def __set_atmpin(self,pin):
+    self.__atmpin=pin
+def access_atmpin(self,pin):
+    self.__set_atmpin(pin)
+
+p1=person()
+p1.access_atmpin(1234)
+p1.get_atmpin() 
+
+#----------------------------
+
 
